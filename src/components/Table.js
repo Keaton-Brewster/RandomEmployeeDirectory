a161c1642b903cad31b47b2efcea4dbaa9b7355b
Keaton-Brewster <keaton.brewster@gmail.com>
2021-04-21 22:05:27 -0500

    added fontawesome React package, and then moved the api call logic into the Table component rather than the TableBody component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,32 @@
-import { Table as BootstrapTable } from "react-bootstrap";
-import TableBody from "./TableBody";
+import {useState, useEffect} from 'react'
+import { Table as BootstrapTable } from 'react-bootstrap';
+import API from "../utils/API"
+import TableBody from './TableBody';
 
 const Table = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(
+        () => {
+            API.getUsers()
+                .then(res => {
+                    setUsers(res.data.results);
+                })
+                .catch(err => console.log(err));
+        }, []);
+
     return (
         <BootstrapTable striped bordered hover>
             <thead>
                 <tr>
-                    <th>Photo</th>
+                    <th>Photo<span></span></th>
                     <th>Name</th>
                     <th>Phone</th>
                     <th>Email</th>
                     <th>DOB</th>
                 </tr>
             </thead>
-            <TableBody />
+            <TableBody users={users}/>
         </BootstrapTable>
     );
 }
--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,25 +1,11 @@
-import { useState, useEffect } from "react"
-import API from "../utils/API"
-
-const TableBody = () => {
-    const [users, setUsers] = useState([]);
-
-    useEffect(
-        () => {
-            API.getUsers()
-                .then(res => {
-                    setUsers(res.data.results);
-                })
-                .catch(err => console.log(err));
-        }, []);
-
+const TableBody = ({users}) => {
     return (
         <tbody>
             {users.map((user, i) => {
                 return (
                     <tr key={i}>
                         <th><img src={user.picture.medium} alt=""></img></th>
-                        <th>{user.name.title} {user.name.first} {user.name.last}</th>
+                        <th>{user.name.first} {user.name.last}</th>
                         <th>{user.cell}</th>
                         <th>{user.email}</th>
                         <th>{user.dob.date}</th>
