7400838827b2b595a97b73dc81b48b9869bb66d6
Keaton-Brewster <keaton.brewster@gmail.com>
2021-04-22 23:41:17 -0500

    got the basic toggle working visually. Time to crack down on the functionality

--- examples/SortToggle.js
+++ examples/SortToggle.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
 
 const SortToggle = ({ type, handleSort, icon }) => {
     return (
--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,52 +1,75 @@
 import React, { useContext, useEffect, useState } from 'react';
-import SortToggle from './SortToggle';
-import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown, faSortUp, faSort } from '@fortawesome/free-solid-svg-icons'
 
 
 import './fontawesome.css'
 const TableHeader = () => {
     // Need to write a custom hook and set up state for the sort optinos, then from the Reducer function maybe I can set up the array.sort for the employee state?
+    const [sort, setSort] = useState({
+        name: 'default',
+        dob: 'default'
+    });
 
-
-
-    const handleSort = event => {
-        console.log(event.target)
-        let name = false;
-        let dob = false;
-
-        switch (event.target.value) {
-            case 'name':
-                name === false ?
-                    name = true :
-                    name = false;
-                dob = false
-                console.log(name)
-                break;
-            case 'dob':
-                dob === false ?
-                    dob = true :
-                    dob = false;
-                name = false
-                console.log(dob)
-                break;
-            default:
-                throw new Error('No value was read from the event.target. event.target must have a value of either "name" or "dob"');
+    const sortTypes = {
+        up: {
+            icon: faSortUp,
+            fn: (a, b) => a.net_worth - b.net_worth
+        },
+        down: {
+            icon: faSortDown,
+            fn: (a, b) => b.net_worth - a.net_worth
+        },
+        default: {
+            icon: faSort,
+            fn: (a, b) => a
         }
     }
 
-    const nameSort =  <SortToggle handleSort={handleSort} type='name' icon={faCaretDown}/>
-    const emailSort = <SortToggle handleSort={handleSort} type='name' icon={faCaretDown}/>
-    const dobSort = <SortToggle handleSort={handleSort} type='dob' icon={faCaretDown}/>
+    const onSortChange = (type) => {
+        let nextSort;
+
+        switch (type) {
+            case 'name':
+                if (sort.name === 'down') nextSort = 'up';
+                else if (sort.name === 'up') nextSort = 'default';
+                else if (sort.name === 'default') nextSort = 'down';
+                setSort({ 
+                    name: nextSort,
+                    dob: 'default'
+                });
+                break;
+            case 'dob':
+                if (sort.dob === 'down') nextSort = 'up';
+                else if (sort.dob === 'up') nextSort = 'default';
+                else if (sort.dob === 'default') nextSort = 'down';
+                setSort({
+                    name: 'default',
+                    dob: nextSort
+                });
+                break;
+            default:
+                throw new Error();
+        }
+    }
 
     // Need to create a whole ass component for the button
     return (
         <thead className="bg-dark">
             <tr className="text-light">
                 <th className="pb-3"> Photo </th>
-                <th>Name {nameSort}</th>
+                <th>Name
+                <button onClick={() => onSortChange('name')}>
+                        <FontAwesomeIcon className="fa" icon={sortTypes[sort.name].icon} />
+                    </button>
+                </th>
                 <th className="pb-3">Phone</th>
-                <th>Email {emailSort}</th>
-                <th>DOB {dobSort}</th>
+                <th>Email</th>
+                <th>DOB
+                <button onClick={() => onSortChange('dob')}>
+                        <FontAwesomeIcon className="fa" icon={sortTypes[sort.dob].icon} />
+                    </button>
+                </th>
             </tr>
         </thead>
     );
