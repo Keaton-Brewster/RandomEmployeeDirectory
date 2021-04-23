3ce95733b64d87fa7d103be904183b6c07c86d03
Keaton-Brewster <keaton.brewster@gmail.com>
2021-04-23 09:44:06 -0500

    made it so that search results don't include email. Only first and last name

--- src/utils/useSearch.js
+++ src/utils/useSearch.js
@@ -1,4 +1,7 @@
-import { useEffect, useState } from 'react';
+import {
+    useEffect,
+    useState
+} from 'react';
 
 // Accepting a duplicate of the imutable users array, and the search state for running the filter params on the array of users
 const useSearch = (users, searchValue) => {
@@ -7,14 +10,17 @@ const useSearch = (users, searchValue) => {
 
     useEffect(() => {
         // Imediately, if the searchvalue is anything less than one character, there is no point in filtering the array
-        if (searchValue === undefined) return
-
+        if (searchValue === undefined) {
+            setSearchedUsers(undefined)
+        }
         // Simple filter by name
-        const results = users.filter(user => {
-            const searchCredentials = user.name.first.toLowerCase() + user.name.last.toLowerCase() + user.email.toLowerCase();
-            return searchCredentials.includes(searchValue.toLowerCase())
-        })
-        setSearchedUsers(results)
+        else {
+            const results = users.filter(user => {
+                const searchCredentials = user.name.first.toLowerCase() + user.name.last.toLowerCase();
+                return searchCredentials.includes(searchValue.toLowerCase())
+            })
+            setSearchedUsers(results)
+        }
     }, [users, searchValue])
 
     return searchedUsers
